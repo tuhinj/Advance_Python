0a215878d7df928397d29dd3eb600647e65b0071
tuhinj <tuhinjoy5858@gmail.com>
2022-10-19 04:33:08 +0600

    update code

--- class-2/data_type.py
+++ class-2/data_type.py
@@ -1,15 +1,49 @@
 # introduction to python data types...
     # str = 'joy', "hello there"
+name = 'Tuhin'
+fullName = "Maynuddin Tuhin Joy"
+print('str:',name, fullName)
     # int = 19, 1, 12224
+a = 10
+b = 234
+print('int:', a, b)
+
     # float = 1.3, 34.5857843098
+c = 23.3
+d = 2.4452345
+print('float:', c, d)
+
     # complex = 23j, 54j
-    # list = ['joy', 56, 3.45464, 1j,['tuhin', 'joy']]
-    # tuple = ('joy', 57, 4.34,['joy',57])
+e = 24j
+f = 3j + 3
+print('Complex:', e, f)
+
+    # list // it can be change (mutable)
+lis = ['joy', 56, 3.45464, 1j,['tuhin', 'joy']]
+print('list:',lis)
+
+    # tuple // it cannot be change (immutable)
+tup = ('joy', 57, 4.34,['joy',57])
+print('tuple:',tup)
+
     # range = range(0, 13, 3) //1st number for range start, //2nd number for range end, //3rd number set range...
-    # dict = {'name': 'joy', 'age': 23, 'family': [{'father': 'Jahangir', 'Mother': 'Monalisa'}]} //in dict have two type key and value..
-    # set = {'Rohim', 'Korim', 'Joy', 'Shanto', 'Joy', 'Shanto'} //in set same data type cann't appliabal...
-    # frozenset = (('joy', 100, 2.3, 3)) //in frozenset cann't apply duplicate value...
+    # dict //in dict have two type key and value..
+dic = {'name': 'joy', 'age': 23, 'family': [{'father': 'Jahangir', 'Mother': 'Monalisa'}]} 
+print('Dict:',dic)
+
+    # set //in set same data cann't appliabal...
+s = {'Rohim', 'Korim', 'Joy', 'Shanto', 'Joy', 'Shanto'}
+print('Set:',s)
+
+    # frozenset //in frozenset cann't apply duplicate value...
+fs = (('joy', 100, 2.3, 3))
+print('frozenset:',fs)
+
     # bool = True, False
+name = 'joy'
+if name == joy:
+    print(True)
+else:
     # bytes, bytearray, memoryview...
     # none //js in null python none...
 
@@ -28,5 +62,4 @@
 #Logical Operators...
     # and, or, not
 # Identity Operators...
-    # is, is not
-    
+gi
