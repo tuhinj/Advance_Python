3516b005d72dc09ef2ece5b8a89516028a01f521
tuhinj <tuhinjoy5858@gmail.com>
2022-10-21 21:00:13 +0600

    update code

--- class-2/data_type.py
+++ class-2/data_type.py
@@ -1,43 +1,43 @@
 # introduction to python data types...
     # str = 'joy', "hello there"
-name = 'Tuhin'
-fullName = "Maynuddin Tuhin Joy"
-print('str:',name, fullName)
+# name = 'Tuhin'
+# fullName = "Maynuddin Tuhin Joy"
+# print('str:',name, fullName)
     # int = 19, 1, 12224
-a = 10
-b = 234
-print('int:', a, b)
+# a = 10
+# b = 234
+# print('int:', a, b)
 
     # float = 1.3, 34.5857843098
-c = 23.3
-d = 2.4452345
-print('float:', c, d)
+# c = 23.3
+# d = 2.4452345
+# print('float:', c, d)
 
     # complex = 23j, 54j
-e = 24j
-f = 3j + 3
-print('Complex:', e, f)
+# e = 24j
+# f = 3j + 3
+# print('Complex:', e, f)
 
     # list // it can be change (mutable)
-lis = ['joy', 56, 3.45464, 1j,['tuhin', 'joy']]
-print('list:',lis)
+# lis = ['joy', 56, 3.45464, 1j,['tuhin', 'joy']]
+# print('list:',lis)
 
-    # tuple // it cannot be change (immutable)
-tup = ('joy', 57, 4.34,['joy',57])
-print('tuple:',tup)
+#     # tuple // it cannot be change (immutable)
+# tup = ('joy', 57, 4.34,['joy',57])
+# print('tuple:',tup)
 
     # range = range(0, 13, 3) //1st number for range start, //2nd number for range end, //3rd number set range...
     # dict //in dict have two type key and value..
-dic = {'name': 'joy', 'age': 23, 'family': [{'father': 'Jahangir', 'Mother': 'Monalisa'}]} 
-print('Dict:',dic)
+# dic = {'name': 'joy', 'age': 23, 'family': [{'father': 'Jahangir', 'Mother': 'Monalisa'}]} 
+# print('Dict:',dic)
 
-    # set //in set same data cann't appliabal...
-s = {'Rohim', 'Korim', 'Joy', 'Shanto', 'Joy', 'Shanto'}
-print('Set:',s)
+#     # set //in set same data cann't appliabal...
+# s = {'Rohim', 'Korim', 'Joy', 'Shanto', 'Joy', 'Shanto'}
+# print('Set:',s)
 
-    # frozenset //in frozenset cann't apply duplicate value...
-fs = (('joy', 100, 2.3, 3))
-print('frozenset:',fs)
+#     # frozenset //in frozenset cann't apply duplicate value...
+# fs = (('joy', 100, 2.3, 3))
+# print('frozenset:',fs)
 
     # bool = True, False
     # bytes, bytearray, memoryview...
@@ -45,9 +45,9 @@ print('frozenset:',fs)
 
 # Arithmetic Operators...
     # +, -, *, /, %, **,//
-n1 = 10
-n2 = 3
-print('All Arithmetic Operation:',n1 + n2, n1 - n2, n1 * n2, n1 / n2, n1 % n2, (n1 + n2)**2, n1 // n2)
+# n1 = 10
+# n2 = 3
+# print('All Arithmetic Operation:',n1 + n2, n1 - n2, n1 * n2, n1 / n2, n1 % n2, (n1 + n2)**2, n1 // n2)
     # PEMDAS
     # P = parenthesis ()
     # E = Exponent **
@@ -59,16 +59,61 @@ print('All Arithmetic Operation:',n1 + n2, n1 - n2, n1 * n2, n1 / n2, n1 % n2, (
 # Assignment Operators...
     #=, +=, -=, /=, %/, //=, **=
     # age = age + 2 or age += 2
-age = 20 
-age += 2
-print(age)
+# age = 20 
+# age += 2
+# print(age)
+
+# - Comparison operators
+        # ==, !=, >, <, >=, <=
+# a = 12
+# print(a == a)
+# print(a != a)
+# print(a < 34)
+# print(a > 45)
+# print(a >= 13)
+# print(a <= 34)
 
 #Logical Operators...
     # and, or, not
-joy = 'worker'
-if joy == 'student' or joy == 'teacher':
-    print('Get parmite to access the University')
-else: print('Sorry!')
+# joy = 'worker'
+# if joy == 'student' or joy == 'teacher':
+#     print('Get parmite to access the University')
+# else: print('Sorry!')
 
 # Identity Operators...
     # is, is not
+# print(joy is joy)
+
+# - Membership operators
+    # in, not in
+students = ["Rahim", "Karim", "Sajal"]
+print("Rahim" in students)
+
+# - Bitwise operators
+
+# operator precendence in Python
+
+# input
+    # name = input("What's your name? :")
+    # print("Hi " + name + ". How are you?")
+
+# output
+    # print("HELLO","World","HI", sep="-")
+
+    # print("Hi there", end="*")
+    # print("How are you")
+
+# variables
+    # sum_of_numbers = 3 + 4 + 6
+
+    #     # pascal case = StudentOneName
+
+    #     # camel case = studentOneName
+
+    #     # snake case = student_one_name # used for declaring variables
+
+    # print(sum_of_numbers)
+
+# comments
+    #I am a student
+    #jksdfjdsklfjdsfkldsfjsdf
---
+++ class-3/introduction-string.py
@@ -0,0 +1,53 @@
+# introduction to string
+# a = ["hello", "hi", 1]
+# a[1] = "bye"
+# print(a)
+# b = ("hello", "hi", 3, 4)
+# print(b)
+c = "Hello world from ugv"
+# string slicing
+# print(c[1], c[2], c[3], sep="")
+# print(c[-8:-4])
+# string modification
+# upper(), lower(), strip(), replace(), split()
+# print(c.upper())
+# c = c.upper()
+# c = c.title()
+# c = c.replace("world", "barishal").lstrip().title()
+# d = "Hello-world-from-ugv"
+# d = d.split("-")
+# d = "-".join(d)
+# print(d)
+# print(" ".join(c))
+# string concatenation
+e = "Hello"
+f = "world"
+# g = e + " " + f
+# print(e + " " + f)
+# formatting string
+# print("{1} {0} from ugv".format(e, f))
+# print("{b} {a} from ugv".format(a = "Hello", b = "world"))
+# print(f"{e} {f} from ugv")
+# string methods
+# capitalize()
+# startswith()
+# print("Hello world from ugv".startswith("ugv"))
+# endswith()
+# find()
+h = "Hello woarld world"
+print(h.find("world"))
+# replace()
+# isalnum()
+# islower()
+# isupper()
+# split()
+# join()
+# strip()
+# lstrip()
+# rstrip()
+# upper()
+# lower()
+# swapcase()
+# i = "Hello world"
+# print(i.swapcase())
+# title()
