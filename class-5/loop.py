1104c33f9d7e930f7bfbe34c91166a2f3a19b92a
tuhinj <tuhinjoy5858@gmail.com>
2022-11-02 02:36:01 +0600

    code update

---
+++ class-5/loop.py
@@ -0,0 +1,16 @@
+# Loop (while, for)
+
+a = 0
+
+# while True:
+#     print(a)
+#     a += 1
+
+# while a <= 50:
+#     a += 1
+#     print(a)
+
+student = ['joy', 'azmal', 'rahat']
+
+for student in enumerate(student):
+    print(student)
