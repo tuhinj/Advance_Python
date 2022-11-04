3dcab61e2dd5b9863a45d602d8ebd1829261880c
tuhinj <tuhinjoy5858@gmail.com>
2022-11-05 00:30:02 +0600

    code add

---
+++ Problem solving/12_beecrowd.py
@@ -0,0 +1,14 @@
+'''
+Make a program that reads three floating point values: A, B and C. Then, calculate and show:
+a) the area of the rectangled triangle that has base A and height C.
+b) the area of the radius's circle C. (pi = 3.14159)
+c) the area of the trapezium which has A and B by base, and C by height.
+d) the area of ​the square that has side B.
+e) the area of the rectangle that has sides A and B.
+
+Input
+The input file contains three double values with one digit after the decimal point.
+
+Output
+The output file must contain 5 lines of data. Each line corresponds to one of the areas described above, always with a corresponding message (in Portuguese) and one space between the two points and the value. The value calculated must be presented with 3 digits after the decimal point.
+'''
