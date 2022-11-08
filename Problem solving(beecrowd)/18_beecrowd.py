e9840dc61454b455f32300515d0e7bbb17783903
tuhinj <tuhinjoy5858@gmail.com>
2022-11-08 12:44:28 +0600

    code add

---
+++ Problem solving(beecrowd)/18_beecrowd.py
@@ -0,0 +1,16 @@
+'''  
+In this problem you have to read an integer value and calculate the smallest possible number of banknotes in which the value may be decomposed. The possible banknotes are 100, 50, 20, 10, 5, 2 e 1. Print the read value and the list of banknotes.
+
+Input
+The input file contains an integer value N (0 < N < 1000000).
+
+Output
+Print the read number and the minimum quantity of each necessary banknotes in Portuguese language, as the given example. Do not forget to print the end of line after each line, otherwise you will receive “Presentation Error”.
+'''
+
+N = int(input())
+
+
+if 0 < N <1000000:
+    while 
+    print()
