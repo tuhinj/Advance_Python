ace9cbd28f0abb00cde476f778de4b42a52027c8
tuhinj <tuhinjoy5858@gmail.com>
2022-11-14 13:58:38 +0600

    code pest

--- class-6/class6.py
+++ class-6/class6.py
@@ -1,37 +1,37 @@
-a = [1, 2, 3, 4, 5, 6]
+# a = [1, 2, 3, 4, 5, 6]
 
-a.append(7)
+# a.append(7)
 
-print(a)
+# print(a)
 
-a.pop()
+# a.pop()
 
-print(a)
+# print(a)
 
-a.pop(2)
+# a.pop(2)
 
-print(a)
+# print(a)
 
-b = [1, 2, 5, 3, 4, 5, 6, 5]
+# b = [1, 2, 5, 3, 4, 5, 6, 5]
 
-c = ["hello", "world", "ugv"]
+# c = ["hello", "world", "ugv"]
 
-c.remove("world")
+# c.remove("world")
 
-print(c)
+# print(c)
 
-b.remove(5)
+# b.remove(5)
 
-print(b)
+# print(b)
 
-d = [1, 2, 3]
+# d = [1, 2, 3]
 
-e = [4, 5, 6]
+# e = [4, 5, 6]
 
-f = d + e # concatenation
+# f = d + e # concatenation
 
-print(d)
-print(e)
+# print(d)
+# print(e)
 
 # print("F is now: ",f)
 
@@ -39,67 +39,67 @@ print(e)
 
 # print("d is now: ",d)
 
-g = [3, 8, 7, 2, 9]
+# g = [3, 8, 7, 2, 9]
 
-g.sort()
+# g.sort()
 
-print(g)
+# print(g)
 
 h = ["hello", "Abdul", "zohir", "akram"]
 
-# h.sort(reverse=True)
+h.sort(reverse=True)
 
 h.reverse()
 
-# print(len({"a": 3, "b": 5}))
-# print(len("hello"))
+print(len({"a": 3, "b": 5}))
+print(len("hello"))
 
-# h.clear()
+h.clear()
 
-# i = h
+i = h
 
-# i = h.copy()
+i = h.copy()
 
-# i[1] = "Rahim"
+i[1] = "Rahim"
 
-# print(i)
-# print(h)
+print(i)
+print(h)
 
-# j = (1, 2, 3, 4)
-# k = (9, 8, 6)
+j = (1, 2, 3, 4)
+k = (9, 8, 6)
 
-# l = j + k
+l = j + k
 
-# print(l)
+print(l)
 
-# def get_students():
-#     # doing lots of operation
-#     return ("Rahim", "Karim", "Abdul")
+def get_students():
+    # doing lots of operation
+    return ("Rahim", "Karim", "Abdul")
 
-# student1, *otherStudents = get_students()
-# # student1, student2, student3 = (1, 2, 3)
+student1, *otherStudents = get_students()
+# student1, student2, student3 = (1, 2, 3)
 
-# print(otherStudents)
+print(otherStudents)
 
-# m = ("Rahim", "Jakaria", "Siam", "Jakaria")
+m = ("Rahim", "Jakaria", "Siam", "Jakaria")
 
 
-# print(m.index("Siam"))
+print(m.index("Siam"))
 
-# print(m.count("Jakaria"))
+print(m.count("Jakaria"))
 
-# names = ["arif", "saikat", "rabbi", "masud"]
+names = ["arif", "saikat", "rabbi", "masud"]
 
-# for index, name in enumerate(names):
-#     # print(name.capitalize(), " ", index)
-#     names[index] = name.capitalize()
+for index, name in enumerate(names):
+    # print(name.capitalize(), " ", index)
+    names[index] = name.capitalize()
 
-# print(names)
+print(names)
 
-# # names = [name + " - eve" for name in names]
+# names = [name + " - eve" for name in names]
 
-# for name in names:
-#     print(name)
+for name in names:
+    print(name)
 
 names = ["arif", "saikat", "rabbi", "masud"]
 
