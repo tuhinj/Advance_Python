498d0816c35906138b9e28132b62f71a583b598d
tuhinj <tuhinjoy5858@gmail.com>
2022-11-14 16:06:26 +0600

    code pest

--- class-6/class6.py
+++ class-6/class6.py
@@ -45,67 +45,69 @@
 
 # print(g)
 
-h = ["hello", "Abdul", "zohir", "akram"]
+# h = ["hello", "Abdul", "zohir", "akram"]
 
-h.sort(reverse=True)
+# h.sort(reverse=True)
+# print(h)
+# h.reverse()
+# print(h)
 
-h.reverse()
+# print(len({"a": 3, "b": 5}))
+# print(len("hello"))
 
-print(len({"a": 3, "b": 5}))
-print(len("hello"))
+# h.clear()
 
-h.clear()
+# i = h
 
-i = h
+# i = h.copy()
 
-i = h.copy()
+# i[1] = "Rahim"
 
-i[1] = "Rahim"
+# print(i)
+# print(h)
 
-print(i)
-print(h)
+# j = (1, 2, 3, 4)
+# k = (9, 8, 6)
 
-j = (1, 2, 3, 4)
-k = (9, 8, 6)
+# l = j + k
 
-l = j + k
+# print(l)
+# print(j, k)
 
-print(l)
-
-def get_students():
+# def get_students():
     # doing lots of operation
-    return ("Rahim", "Karim", "Abdul")
+    # return ("Rahim", "Karim", "Abdul")
 
-student1, *otherStudents = get_students()
+# student1, *otherStudents = get_students()
 # student1, student2, student3 = (1, 2, 3)
 
-print(otherStudents)
+# print(student1, student2, student3)
 
-m = ("Rahim", "Jakaria", "Siam", "Jakaria")
+# m = ("Rahim", "Jakaria", "Siam", "Jakaria")
 
 
-print(m.index("Siam"))
+# print(m.index("Siam"))
 
-print(m.count("Jakaria"))
+# print(m.count("Jakaria"))
 
 names = ["arif", "saikat", "rabbi", "masud"]
 
 for index, name in enumerate(names):
-    # print(name.capitalize(), " ", index)
-    names[index] = name.capitalize()
+    print(name.capitalize(), " ", index)
+    # names[index] = name.capitalize()
 
 print(names)
 
-# names = [name + " - eve" for name in names]
+# # names = [name + " - eve" for name in names]
 
-for name in names:
-    print(name)
+# for name in names:
+#     print(name)
 
-names = ["arif", "saikat", "rabbi", "masud"]
+# names = ["arif", "saikat", "rabbi", "masud"]
 
-# names = [name.upper() for name in names if "rabbi" == name]
+# # names = [name.upper() for name in names if "rabbi" == name]
 
-names = [name.upper() if name == "rabbi" else name for name in names]
+# names = [name.upper() if name == "rabbi" else name for name in names]
 
-print("NAMES: ", names)
+# print("NAMES: ", names)
 
