aa2cfa57f6d30298136df99e1986edaeff36a47a
tuhinj <tuhinjoy5858@gmail.com>
2022-11-16 20:53:12 +0600

    update code

--- class-8/class8_practice_function.py
+++ class-8/class8_practice_function.py
@@ -3,4 +3,18 @@
 def print_hello():
     print("hello")
 
-print_hello()
+# print_hello()
+
+def print_hello(neme):
+    print(f"Hello {neme}")
+
+# print_hello("joy")
+
+students = []
+def set_winers(first_winner, second_winner, third_winner):
+    students["first_winner"] = first_winner
+    students["second_winner"] = second_winner
+    students["third_winner"] = third_winner
+
+set_winers("Joy", "pronoy","Rahat")
+print(students)
