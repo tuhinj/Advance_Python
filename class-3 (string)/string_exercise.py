404a1910d41799538821578136e437388e9125d0
tuhinj <tuhinjoy5858@gmail.com>
2022-11-20 01:54:42 +0600

    problem solving

--- class-3 (string)/string_exercise.py
+++ class-3 (string)/string_exercise.py
@@ -30,7 +30,16 @@ print(b.replace("world", "Bangladesh").strip().title())
 
 print(" ".join(b).strip())
 
-print()
+# Concatination...
+
+print(b.strip() + a)
+
+print(a,b.strip())
+
+print("Hello I'm From {f}, My name is {n}". format(f = "Bangladesh", n = "Maynuddin Tuhin Joy"))
+
+print("{0} {1} from ugv".format(b.strip().capitalize(), a))
+
 
 
 
