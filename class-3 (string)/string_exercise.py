cb6376ca584555182ffabaa887bc67fd8aa06a43
tuhinj <tuhinjoy5858@gmail.com>
2022-11-20 02:02:04 +0600

    problem solving

--- class-3 (string)/string_exercise.py
+++ class-3 (string)/string_exercise.py
@@ -30,6 +30,12 @@ print(b.replace("world", "Bangladesh").strip().title())
 
 print(" ".join(b).strip())
 
+e = "HELLO"
+
+print(e.isupper(),"\n",a.isupper())
+
+print(e.isalnum(),a.isalnum())
+
 # Concatination...
 
 print(b.strip() + a)
@@ -39,6 +45,8 @@ print(a,b.strip())
 print("Hello I'm From {f}, My name is {n}". format(f = "Bangladesh", n = "Maynuddin Tuhin Joy"))
 
 print("{0} {1} from ugv".format(b.strip().capitalize(), a))
+
+print(f"Hi: {b.capitalize().strip()}, \nWhat is your name: {a}")
 
 
 
