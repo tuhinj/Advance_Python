6cce5e80c348cb0441044ea13f7b2a34a75639af
tuhinj <tuhinjoy5858@gmail.com>
2022-11-20 02:52:22 +0600

    problem solving

---
+++ class-5 (for...while...'loop')/loop_practice.py
@@ -0,0 +1,22 @@
+a = 0
+
+# while True:
+    # print(a)
+    # a = a + 1
+
+while a <= 49:
+    a += 1
+    print(a)
+
+while True:
+    a+=1
+    print(a)
+    if a == 100:
+        print("Done")
+        break
+students = ["Maynuddin", "Tuhin", "Joy"]
+for student in students:
+    if student == "Tuhin":
+        print(student)
+        break
+
