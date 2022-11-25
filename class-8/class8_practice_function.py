44ab44892091718fe02f647f1e31f3a66b6d06ad
tuhinj <tuhinjoy5858@gmail.com>
2022-11-25 11:31:29 +0600

    code update

--- class-8/class8_practice_function.py
+++ class-8/class8_practice_function.py
@@ -10,16 +10,15 @@ def print_hello(neme):
 
 print_hello("joy")
 
-def print_name(name):
-    input_name = input()
-    print("Hi",input_name,name)
+# def print_name(name):
+#     input_name = input()
+#     print("Hi",input_name,name)
 
-print_name("joy")
+# print_name("joy")
 
 studen_list = []
 def print_studens(*name):
     print(name)
 
 print_studens("Joy","Tuhin","Maynuddin")
-
 print(studen_list)
