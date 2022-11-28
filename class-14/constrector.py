457264cec72d862848ad6363fc428b82a294d3c0
tuhinj <tuhinjoy5858@gmail.com>
2022-11-28 18:12:16 +0600

    code add by practice class

--- class-14/constrector.py
+++ class-14/constrector.py
@@ -12,11 +12,17 @@ class Animal:
         return f"{self.name} is Barking"
 
 class Dog(Animal):
-    pass
+    
     # def __init__(self,name, age, color):
     #     self.name = name
     #     self.age = age
     #     self.color = color
+    def __init__(self, name, age, color, is_active):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.is_active = is_active
+
 
 class cat(Animal):
     pass
@@ -34,7 +40,8 @@ class cow(Animal):
     #     self.age = age
     #     self.color = color
 
-animal = Dog("Tom", 1, "Black")
-print(animal.name, animal.age, animal.color,"and", animal.walk())
+animal = Dog("Tom", 1, "Black", True)
+print(animal.name, animal.age, animal.color, animal.is_active,"and", animal.walk())
 
-animal_cat = cat("Pussi", 3.5, "White","/n")
+animal_cat = cat("Pussi", 3.5, "White")
+print(animal_cat.name, animal_cat.age, animal_cat.color, "and", animal_cat.walk())
