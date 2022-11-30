ce6e8c64e041277e42f81e5e99bd31ebbdd81c42
tuhinj <tuhinjoy5858@gmail.com>
2022-11-30 08:37:12 +0600

    code add and update

--- class-8/class8_practice.py
+++ class-8/class8_practice.py
@@ -73,3 +73,14 @@ def print_list (num):
         num.pop()
     return num
 print_list(list1)
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(15)
