5f1fdf46f42093c21329fc2f33e9185bb732beda
tuhinj <tuhinjoy5858@gmail.com>
2022-12-01 14:39:07 +0600

    code add and update

--- class-10/class_10.py
+++ class-10/class_10.py
@@ -3,6 +3,29 @@
 #         cash_in_hand -= 100
 #         calculate_total = cash_in_bank + cash_in_hand
 #     return cash_in_hand 
-import class_11
+# import class_11
 
-print(class_11.calculate_balance(10000,850))
+# print(class_11.calculate_balance(10000,850))
+# num = 5 + '5'
+
+# try:
+#     num = 5 + int("5")
+# except ValueError:
+#     print("Something wrong")
+
+# lis = [23, 29, 0, 5]
+# result = lis[1]/lis[2]
+
+# print(result)
+
+try:
+    lis = [23, 29, 0, 5]
+    result = lis[1]/lis[2]
+
+    print(result)
+except ZeroDivisionError:
+    print("It's not possible")
+finally:
+    print("Done!")
+
+print("Hello world!")
--- test.py
+++ test.py
@@ -1,9 +1,34 @@
-a = {"name":"joy", "age": 12}
+# a = {"name":"joy", "age": 12}
 
-print(a.values())
+# # import pdb; pdb.set_trace()
 
-print(a.keys())
+# print(a.values())
 
-print(a.keys())
+# print(a.keys())
+
+# print(a.keys())
 
 
+# num = 5 + '5'
+
+# try:
+#     num = 5 + int("5")
+# except ValueError:
+#     print("Something wrong")
+
+# lis = [23, 29, 0, 5]
+# result = lis[1]/lis[2]
+
+# print(result)
+
+try:
+    lis = [23, 29, 0, 5]
+    result = lis[1]/lis[2]
+
+    print(result)
+except ZeroDivisionError:
+    print("It's not possible")
+finally:
+    print("Done!")
+
+print("Hello world!")
