655dca678e1c1cc923843bc0f7c0ec44a1c34c51
tuhinj <tuhinjoy5858@gmail.com>
2022-12-01 18:04:32 +0600

    code add and update

--- test.py
+++ test.py
@@ -26,46 +26,78 @@
 # ID: 800966229
 # ITCS 6114
 
-import sys
-from sys import argv
-from struct import *
+# import sys
+# from sys import argv
+# from struct import *
 
-# taking the input file and the number of bits from command line
-# defining the maximum table size
-# opening the input file
-# reading the input file and storing the file data into data variable
-input_file, n = argv[1:]                
-maximum_table_size = pow(2,int(n))      
-file = open(input_file)                 
-data = file.read()                      
+# # taking the input file and the number of bits from command line
+# # defining the maximum table size
+# # opening the input file
+# # reading the input file and storing the file data into data variable
+# input_file, n = argv[1:]                
+# maximum_table_size = pow(2,int(n))      
+# file = open(input_file)                 
+# data = file.read()                      
 
-# Building and initializing the dictionary.
-dictionary_size = 256                   
-dictionary = {chr(i): i for i in range(dictionary_size)}    
-string = ""             # String is null.
-compressed_data = []    # variable to store the compressed data.
+# # Building and initializing the dictionary.
+# dictionary_size = 256                   
+# dictionary = {chr(i): i for i in range(dictionary_size)}    
+# string = ""             # String is null.
+# compressed_data = []    # variable to store the compressed data.
 
-# iterating through the input symbols.
-# LZW Compression algorithm
-for symbol in data:                     
-    string_plus_symbol = string + symbol # get input symbol.
-    if string_plus_symbol in dictionary: 
-        string = string_plus_symbol
-    else:
-        compressed_data.append(dictionary[string])
-        if(len(dictionary) <= maximum_table_size):
-            dictionary[string_plus_symbol] = dictionary_size
-            dictionary_size += 1
-        string = symbol
+# # iterating through the input symbols.
+# # LZW Compression algorithm
+# for symbol in data:                     
+#     string_plus_symbol = string + symbol # get input symbol.
+#     if string_plus_symbol in dictionary: 
+#         string = string_plus_symbol
+#     else:
+#         compressed_data.append(dictionary[string])
+#         if(len(dictionary) <= maximum_table_size):
+#             dictionary[string_plus_symbol] = dictionary_size
+#             dictionary_size += 1
+#         string = symbol
 
-if string in dictionary:
-    compressed_data.append(dictionary[string])
+# if string in dictionary:
+#     compressed_data.append(dictionary[string])
 
-# storing the compressed string into a file (byte-wise).
-out = input_file.split(".")[0]
-output_file = open(out + ".lzw", "wb")
-for data in compressed_data:
-    output_file.write(pack('>H',int(data)))
+# # storing the compressed string into a file (byte-wise).
+# out = input_file.split(".")[0]
+# output_file = open(out + ".lzw", "wb")
+# for data in compressed_data:
+#     output_file.write(pack('>H',int(data)))
     
-output_file.close()
-file.close()
+# output_file.close()
+# file.close()
+
+name = "Maynuddin tuhin joy"
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.split(" "))
+print(name.strip())
+print(name[:9])
+print(name[10:15])
+print(name[10:])
+print(name[-3:])
+
+names = ['Tuhin', 'Joy', 'Maynuddin', 'Pronoy']
+name2 = [1, 2, 6, 5, 3, 4]
+
+names.extend(name2)
+print(names)
+
+names.append("Maliha")
+print(names)
+
+names.pop()
+print(names)
+
+names.insert(3, "Zihad")
+print(names)
+
+names.remove("Pronoy")
+print(names)
+
