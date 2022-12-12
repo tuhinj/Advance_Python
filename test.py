308634534115202ce90f9646aac29d0cf970bff5
tuhinj <tuhinjoy5858@gmail.com>
2022-12-12 19:34:41 +0600

    updated

--- test.py
+++ test.py
@@ -199,8 +199,8 @@ class Dog(Animal):
 class Cat(Animal):
     # def __init__(self, name, age, color):
     def __init__(self, sound):
-        # self.sound = sound
-        print(f'The cat sound {sound}"')
+        self.sound = sound
+        # print(f'The cat sound {sound}"')
         # self.name = name
         # self.age = age
         # self.color = color
