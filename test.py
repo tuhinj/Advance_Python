999ce47c8b536d97afbb12302d99e34052a145fb
tuhinj <tuhinjoy5858@gmail.com>
2023-01-03 00:13:50 +0600

    Let's code and enjoy

--- test.py
+++ test.py
@@ -119,5 +119,22 @@ print(a + b + c)
     # name = "Joy"
     # Sentence = "My name is Maynuddin Tuhin Joy"
 
+'''Python Casting'''
+'''Integers:'''
+x = int(1.4)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+'''Floats:'''
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+'''Strings:'''
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
 # Class 3: (strings Slicing and formatting,string methods, Booleans)
 
