f56f36dd8648fa6bb7a7fb493c7ee614e60b52a8
tuhinj <tuhinjoy5858@gmail.com>
2023-01-02 21:52:54 +0600

    Let's code and enjoy

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+# Class 1:
 print("Hello World!")
+
+# Class 2: (Introduction to python data types,Operators, Operator precedence, Input, Output, Variables, Comments
+'''Text data type: str'''
+string = 'Hello World'
+print(string)
+
+'''Numeric Types:	int, float, complex'''
+intger = 12
+print(intger)
+floating = 12.3
+print(floating)
