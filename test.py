ec6172023016a85cba99c31eee49e1f812c48ad8
tuhinj <tuhinjoy5858@gmail.com>
2023-01-03 01:24:59 +0600

    Let's code and enjoy

--- test.py
+++ test.py
@@ -156,95 +156,95 @@ print(txt.format(name, age))
 print(f"My name is {name}, I'm {age} years old")
 
 '''string methods'''
-# ---Method--------------Description:
-# ===================================
-# capitalize()-> Converts the first character to upper case
+    # ---Method--------------Description:
+    # ===================================
+    # capitalize()-> Converts the first character to upper case
 
-# casefold()-> Converts string into lower case
+    # casefold()-> Converts string into lower case
 
-# center()-> Returns a centered string
+    # center()-> Returns a centered string
 
-# count()-> Returns the number of times a specified value occurs in a string
+    # count()-> Returns the number of times a specified value occurs in a string
 
-# encode()-> Returns an encoded version of the string
+    # encode()-> Returns an encoded version of the string
 
-# endswith()-> Returns true if the string ends with the specified value
+    # endswith()-> Returns true if the string ends with the specified value
 
-# expandtabs()-> Sets the tab size of the string
+    # expandtabs()-> Sets the tab size of the string
 
-# find()-> Searches the string for a specified value and returns the position of where it was found
+    # find()-> Searches the string for a specified value and returns the position of where it was found
 
-# format()->Formats specified values in a string
+    # format()->Formats specified values in a string
 
-# format_map()-> Formats specified values in a string
+    # format_map()-> Formats specified values in a string
 
-# index()	Searches the string for a specified value and returns the position of where it was found
+    # index()	Searches the string for a specified value and returns the position of where it was found
 
-# isalnum()	Returns True if all characters in the string are alphanumeric
+    # isalnum()	Returns True if all characters in the string are alphanumeric
 
-# isalpha()	Returns True if all characters in the string are in the alphabet
+    # isalpha()	Returns True if all characters in the string are in the alphabet
 
-# isdecimal()	Returns True if all characters in the string are decimals
+    # isdecimal()	Returns True if all characters in the string are decimals
 
-# isdigit()	Returns True if all characters in the string are digits
+    # isdigit()	Returns True if all characters in the string are digits
 
-# isidentifier()	Returns True if the string is an identifier
+    # isidentifier()	Returns True if the string is an identifier
 
-# islower()	Returns True if all characters in the string are lower case
+    # islower()	Returns True if all characters in the string are lower case
 
-# isnumeric()	Returns True if all characters in the string are numeric
+    # isnumeric()	Returns True if all characters in the string are numeric
 
-# isprintable()	Returns True if all characters in the string are printable
+    # isprintable()	Returns True if all characters in the string are printable
 
-# isspace()	Returns True if all characters in the string are whitespaces
+    # isspace()	Returns True if all characters in the string are whitespaces
 
-# istitle()	Returns True if the string follows the rules of a title
+    # istitle()	Returns True if the string follows the rules of a title
 
-# isupper()	Returns True if all characters in the string are upper case
+    # isupper()	Returns True if all characters in the string are upper case
 
-# join()	Joins the elements of an iterable to the end of the string
+    # join()	Joins the elements of an iterable to the end of the string
 
-# ljust()	Returns a left justified version of the string
+    # ljust()	Returns a left justified version of the string
 
-# lower()	Converts a string into lower case
+    # lower()	Converts a string into lower case
 
-# lstrip()	Returns a left trim version of the string
+    # lstrip()	Returns a left trim version of the string
 
-# maketrans()	Returns a translation table to be used in translations
+    # maketrans()	Returns a translation table to be used in translations
 
-# partition()	Returns a tuple where the string is parted into three parts
+    # partition()	Returns a tuple where the string is parted into three parts
 
-# replace()	Returns a string where a specified value is replaced with a specified value
+    # replace()	Returns a string where a specified value is replaced with a specified value
 
-# rfind()	Searches the string for a specified value and returns the last position of where it was found
+    # rfind()	Searches the string for a specified value and returns the last position of where it was found
 
-# rindex()	Searches the string for a specified value and returns the last position of where it was found
+    # rindex()	Searches the string for a specified value and returns the last position of where it was found
 
-# rjust()	Returns a right justified version of the string
+    # rjust()	Returns a right justified version of the string
 
-# rpartition()	Returns a tuple where the string is parted into three parts
+    # rpartition()	Returns a tuple where the string is parted into three parts
 
-# rsplit()	Splits the string at the specified separator, and returns a list
+    # rsplit()	Splits the string at the specified separator, and returns a list
 
-# rstrip()	Returns a right trim version of the string
+    # rstrip()	Returns a right trim version of the string
 
-# split()	Splits the string at the specified separator, and returns a list
+    # split()	Splits the string at the specified separator, and returns a list
 
-# splitlines()	Splits the string at line breaks and returns a list
+    # splitlines()	Splits the string at line breaks and returns a list
 
-# startswith()	Returns true if the string starts with the specified value
+    # startswith()	Returns true if the string starts with the specified value
 
-# strip()	Returns a trimmed version of the string
+    # strip()	Returns a trimmed version of the string
 
-# swapcase()	Swaps cases, lower case becomes upper case and vice versa
+    # swapcase()	Swaps cases, lower case becomes upper case and vice versa
 
-# title()	Converts the first character of each word to upper case
+    # title()	Converts the first character of each word to upper case
 
-# translate()	Returns a translated string
+    # translate()	Returns a translated string
 
-# upper()	Converts a string into upper case
+    # upper()	Converts a string into upper case
 
-# zfill()	Fills the string with a specified number of 0 values at the beginning
+    # zfill()	Fills the string with a specified number of 0 values at the beginning
 
 m = "hello world, Hello world"
 print(m.capitalize())
@@ -266,3 +266,16 @@ print(m.strip())
 print(m1.swapcase())
 print(m1.title())
 print(m.upper())
+
+'''Python - Escape Characters'''
+
+    # Code	Result	
+    # \' -> Single Quote	
+    # \\ -> Backslash	
+    # \n -> New Line	
+    # \r -> Carriage Return	
+    # \t -> Tab	
+    # \b -> Backspace	
+    # \f -> Form Feed	
+    # \ooo -> Octal value	
+    # \xhh -> Hex value
