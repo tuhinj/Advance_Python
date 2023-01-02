937e0383ebd5ed611ae7317c3c94255333b2256f
tuhinj <tuhinjoy5858@gmail.com>
2023-01-03 00:24:49 +0600

    Let's code and enjoy

--- test.py
+++ test.py
@@ -137,4 +137,10 @@ y = str(2)    # y will be '2'
 z = str(3.0)  # z will be '3.0'
 
 # Class 3: (strings Slicing and formatting,string methods, Booleans)
-'''strings Slicing and formatting'''
+'''strings Slicing'''
+slic = "Hello World"
+print(slic[0])
+print(slic[-1])
+print(slic[:5])
+
+'''string formatting'''
