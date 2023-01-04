32e263206dae4ff74378078461e9616c556abcb1
tuhinj <tuhinjoy5858@gmail.com>
2023-01-04 08:58:24 +0600

    Let's code and enjoy

--- test.py
+++ test.py
@@ -766,6 +766,39 @@ Dic3 = dict(Dic)
 print(Dic3)
 
 '''Nested Dictionaries'''
-Dic = {"Name":"Joy", "Age":23, "Job":"Student", "Study":"BSC"}
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily)
 
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily1 = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print(myfamily1)
 
