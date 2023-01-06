90ed8e42ab8e8816f6f0ebca5a1a052de2b2a4df
tuhinj <tuhinjoy5858@gmail.com>
2023-01-06 21:46:44 +0600

    code update

--- class-8 (Function)/class8.py
+++ class-8 (Function)/class8.py
@@ -1,78 +1,103 @@
-# def add(a, b):
-#     print(a + b)
+# Class 8: (Getting started with function in Python, Recursive function, Introduction to lambda function in Python, Introduction to built in function in Python, Scopes in Python, Pass statement in python)
+"""Python Functions"""
+def my_function():
+    print("Hello World")
+my_function()
 
-# add(4, 5)
+def full_name(firstn): # Arguments
+    print(firstn + " Joy")
+full_name("Maynuddin")
+full_name("Tuhin")
 
-# add(9, 10)
+def my_name(firstn, lastn):
+    print(firstn,lastn)
+my_name("Maynuddin","Tuhin Joy")
 
-# def addition(a, b):
-#     return a + b
+def fruit(*name): # Make a tuple by this Arguments '*name'
+    print(name)
+fruit("Jackfruit","Lichi","Mango", "Banana")
 
-# print(addition(4, 6))
+def family_member(**name): # Make a Dictionary by this Arguments '**name'
+    print("My family member:",name)
+family_member(Father = "Jamal",Mother = "Jesmin", Son = "Kabir")
 
-# rahims_age = 27
+'''Default Parameter Value'''
+def your_name(name = "Please enter your name"):
+    print("My name is " + name)
+your_name()
+your_name("Joy")
 
+def fruits_name(fruit): # Passing a List as an Argument
+    for x in fruit:
+        print(x)
+fruite_list = ["Banana", "Apple", "Jackfruit"]
+fruits_name(fruite_list)
 
-# def increment_age(year_in_number):
-#     global rahims_age
-#     rahims_age = rahims_age + year_in_number
-#     return rahims_age
+def math(a):
+    return 2 * a
+print("Result:",math(5))
+print("Result:",math(10))
 
-# def increment_age(year_in_number):
-#     global rahims_age
-#     return rahims_age + year_in_number
+def myfunction(): # put in the pass statement to avoid getting an error.
+  pass
 
-# print(increment_age(4))
+"""Recursive function"""
+def rec_fun(k):
+    if (k > 0):
+        result = k + rec_fun(k - 1)
+        print(result)
+    else:
+        result = 0
+    return result
+rec_fun(10)
 
-# boyos = increment_age(4)
+"""LAMBDA"""
+x = lambda a : a + 12
+print("Sum:",x(2))
 
-# print("rahimer boyos: ", boyos)
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
 
+def myfunc(n):
+  return lambda a : a * n
+mydoubler = myfunc(2)
 
+print(mydoubler(10))
 
-# if rahims_age > 30:
-#     print("give him some flour")
+def myfunc(a):
+    return lambda x : x * a
+myfun_num = myfunc(2)
+myfun_num1 = myfunc(4)
 
+print("LAMBDA",myfun_num(12))
+print("LAMBDA",myfun_num1(10))
 
-# increment_age(1)
+"""Scopes"""
+def fun_1():
+    x = 300
+    print(x)
+fun_1()
 
+def f_1():
+    x = 1000
+    def f_2():
+        print(x)
+    f_2()
+f_1()
 
+'''Global variables'''
+x1 = 400
+def func_1():
+    print(x1)
+func_1()
 
-# def print_numbers(list_of_numbers, age):
-#     for number in list_of_numbers:
-#         print(number)
+x2 = 700
+def func_2(num1):
+    global x2
+    a = x2 + num1
+    print("sum:",a)
+func_2(300)
 
-
-# print_numbers([1, 2, 3, 4, 5])
-
-# print("\n")
-
-# print_numbers((3, 4, 2, 4))
-
-
-# def print_names(*names):
-#     for name in names:
-#         print(name)
-
-
-# print_names("monirul", "jannat", "safa", "mizan")
-
-def print_winners(first_winner, second_winner, third_winner):
-    print("Winner is: ", first_winner)
-    print("Runners up is: ", second_winner)
-    print("Third winner is: ", third_winner)
-
-
-
-# print_winners("Rafiq", "Jannat", "Rafi")
-
-# print_winners(second_winner="Jannat", third_winner="Rafi", first_winner="Rafiq")
-
-# def print_winners(**winners):
-
-#     # print(winners)
-#     for key, value in winners.items():
-#         print(key + " => " + value)
-
-# print_winners(first_winner="Rafiq", second_winner="Jannat", third_winner="Rafi")
-
+'''Pass'''
+def function1(word):
+    pass
