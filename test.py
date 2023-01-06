ddbcc3542f687742601fb36f3248ded45a8cfc1b
tuhinj <tuhinjoy5858@gmail.com>
2023-01-07 00:07:38 +0600

    code update

--- test.py
+++ test.py
@@ -929,14 +929,35 @@ print(platform.node())
 print(platform.machine())
 print(platform.python_compiler())
 print(platform.uname())
-
 print(dir(platform))
 
+'''Datetime'''
 import datetime
 print(datetime.datetime.now())
 a = datetime.datetime.now()
 print(a.year)
 print(a.strftime("%A"))
-
 print(datetime.datetime(1999, 7, 6))
 
+'''Math'''
+'build-in math functions'
+a = min(4, 2, 5, -4, -1)
+b = max(4, 2, 5, -4, -1)
+print("Max number:",b,"\nMin number:", a)
+
+c = abs(-4.55654) 
+print("Absolute(positive) value:",c)
+
+d = pow(5, 2)
+print("The value of 5 to the power of 2:", d)
+
+'''The Math Module'''
+import math
+x = math.sqrt(49)
+print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
