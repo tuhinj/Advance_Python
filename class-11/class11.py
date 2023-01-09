6b1a30c542bd4f12ec816349652fa5fb1f205823
tuhinj <tuhinjoy5858@gmail.com>
2023-01-10 01:32:36 +0600

    Updte file and dir

--- class-11/class11.py
+++ class-11/class11.py
@@ -1,101 +1,63 @@
-# file = open("hello.txt", 'r')
+# Class 11: (Iterators, Generators, Decorators)
+"""Iterators"""
+foots = ("Apple", "Banana", "Orange", "Gohaba")
+foot = iter(foots)
 
-# print(file.read())
-# print(file.read())
-# print(file.read())
-# print(file.readline())
-# print(file.readline())
-# print(file.readline())
-# print(file.readlines())
-# text_lines = file.readlines()
-# print(text_lines)
+print(next(foot))
+print(next(foot))
+print(next(foot))
+print(next(foot))
 
+a = "apple"
+b = iter(a)
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
+print(next(b))
 
-# file = open("hello.txt", 'w')
+'''Create an Iterator'''
+class mynum:
+    def __iter__(self):
+        self.a = 1
+        return self
+    
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+class1 = mynum()
+myiter = iter(class1)
 
-# file.write("Hello again 2nd time")
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
 
-# file = open("hello.txt", "a")
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
 
-# file.write("\nHi there")
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
 
-# file = open("hello.txt", 'w')
+myclass = MyNumbers()
+myiter = iter(myclass)
 
-# file = open("hello.txt", 'x') # gives error if file already exists
+for x in myiter:
+  print(x)
 
-# file = open("hi.txt", "a")
-
-# file = open("hello.txt", 'r')
-
-# print(file.readline())
-# print(file.readline())
-# file.seek(1)
-# print(file.readline())
-# print(file.readline())
-
-# file = open("hello.txt", "r+")
-
-# print(file.readline())
-# file.seek(0)
-# file.write("\nWe're learning python\n")
-
-# list_of_students = ["Rahim", "Karim", "Sohan", "Noman", "Ashiq", "Tripti"]
-
-# file = open("student_list.txt", 'a')
-
-# for student in list_of_students:
-#     # file.write(student + "\n")
-#     file.write(f"{student}\n")
-
-
-
-# file = open("hi.txt", 'r')
-
-# print(file.read())
-# file.seek(0)
-# print(file.read())
-# print(file.readline())
-# print(file.readline())
-# print(file.readline())
-# file.seek(0)
-# print(file.readline())
-# print(file.readline())
-# print(file.readlines())
-
-# file = open("hello.txt", 'w')
-
-# file.write("We're writing something\n")
-# file.write("Whats the problem")
-
-# file = open("hello.txt", "a")
-
-# file.write("\nWe're writing again")
-
-# file = open("simple_file.txt", "x")
-
-# file.close()
-
-# with open("student_list.txt", 'r') as file:
-#     print(file.read())
-
-# print("hello")
-
-
-# with open("hi.txt", "r+") as file:
-#     print(file.read())
-#     file.write("Hi there")
-#     # print(file.read())
-
-
-student_list = []
-
-with open("student_list.txt", "r") as file:
-    # for student in file.readlines():
-    #     # print(type(student))
-    #     # print(student[:-1])
-    #     # print(student.replace("\n", ""))
-    #     student_list.append(student.replace("\n", ""))
-
-    student_list = [student.replace("\n", "") for student in file.readlines()]
-
-print(student_list)
+"""Generators"""
