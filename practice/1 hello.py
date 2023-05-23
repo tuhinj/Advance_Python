d9b9ccd2c6ae9fb98a5f107e8b495dc9db83554f
Maynuddin Tuhin Joy <you@example.com>
2023-05-23 11:45:49 +0600

    code update

--- practice/1 hello.py
+++ practice/1 hello.py
@@ -8,25 +8,35 @@
 
 ''' 2. Write a python program that takes a list of integer as input and pirint the sum all of even numbers in the list'''
 
-n_list = input("Enter some number of integer (must be comma separater): ")
+# n_list = input("Enter some number of integer (must be comma separater): ")
 
-n_list = n_list.split(",")
+# n_list = n_list.split(",")
 
-n_list = [int(num) for num in n_list]
+# n_list = [int(num) for num in n_list]
 
-even_sum = sum(num for num in n_list if num % 2 == 0 )
+# even_sum = sum(num for num in n_list if num % 2 == 0 )
 
-odd_sum = sum(num for num in n_list if num % 2 != 0)
+# odd_sum = sum(num for num in n_list if num % 2 != 0)
 
-print(even_sum, odd_sum)
+# print(even_sum, odd_sum)
 
 ''' 3. Write a python program to check if a given string is a palindrome A Palindrome is a word, phrash, number, or other sequence of characters that reatds the same forward and backward'''
 
-def check_palindrome(words):
-    reversed_word = ""
-    for char in words:
-        reversed_word = char + reversed_word
-    return words == reversed_word
+# def check_palindrome(words):
+#     reversed_word = ""
+#     for char in words:
+#         reversed_word = char + reversed_word
+#     return words == reversed_word
 
-words = input("Enter a word: ")
-print(f"{words} is Palindrome: {check_palindrome(words)}")
+# words = input("Enter a word: ")
+# print(f"{words} is Palindrome: {check_palindrome(words)}")
+
+def check_palindrome(strings):
+    reversed_word = ""
+    for chr in strings:
+        reversed_word = chr + reversed_word
+        print(reversed_word)
+    return strings == reversed_word
+
+word = input("Enter any word: ")
+print(f"{check_palindrome(word)}")
